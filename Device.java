50c9413d3ee3036b2a3456aa94f9296075b45062
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2025-04-20 00:50:20 +0000

    updated device "class" to be a proper interface

--- Device.java
+++ Device.java
@@ -1,36 +1,7 @@
-public class Device {
-
-  // Instance Variables
-  private String name;
-  private boolean isOn;
-
-  // Constructor
-  public Device(String name){
-    this.name = name;
-  }
-
-  public Device(){}
-
-  // Accessor methods
-  public String getName(){
-    return this.name;
-  }
-
-  // Class Method Definitions
-  public void turnOn(){}
-
-  public void turnOff(){}
-
-  public boolean isOn(){
-    if (isOn == true){
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-
-
-
-
+// Device Interface for all types of devices
+public interface Device {
+  public void turnOn();
+  public void turnOff();
+  public boolean isOn();
+  public String getName();
 }
